35a1e9e7cca1f57c49ac576fb613b765038cd9cc
asceznyk <asceznyk@gmail.com>
2021-11-03 18:03:42 +0530

    ..

---
+++ summarize.py
@@ -0,0 +1,24 @@
+import sys
+import numpy as np
+
+import torch
+import transformers
+
+from transformers import pipeline
+
+# Open and read the article
+f = open(sys.argv[1], "r", encoding="utf8")
+to_tokenize = f.read()
+
+# Initialize the HuggingFace summarization pipeline
+summarizer = pipeline("summarization")
+summarized = summarizer(to_tokenize, min_length=75, max_length=300)
+
+# Print summarized text
+print(summarized)
+
+#python summarize.py file.txt
+#fuck you bitch go suck on donald trump's dick
+
+
+
